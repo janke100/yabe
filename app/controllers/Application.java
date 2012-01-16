a8e6bf34aab10a6ed61d8380bfaab2278c2e5a21
Milan Stojanovic <stojanovic.milan@gmail.com>
2012-01-16 02:44:15 +0100

    Home page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,17 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-		render();
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
-
+	
+	public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
 }
